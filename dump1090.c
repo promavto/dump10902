6829316d32ef8272196ccdae2f7e4edf0b864510
promavto <promavto@ntmp.ru>
2024-02-12 17:08:10 +0300

    ff

--- dump1090.c
+++ dump1090.c
@@ -1923,13 +1923,43 @@ void uartShowData(void)
 
     int serial_port = open("/dev/ttyAMA0", O_RDWR);
 
-   // struct termios tty;
+    struct termios tty;
+    if (tcgetattr(serial_port, &tty) != 0)
+    {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
 
-     unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r' };
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ECHONL;
+    tty.c_lflag &= ~ISIG;
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+
+    tty.c_oflag &= ~OPOST;
+    tty.c_oflag &= ~ONLCR;
+
+    tty.c_cc[VTIME] = 10;
+    tty.c_cc[VMIN] = 0;
+
+    cfsetispeed(&tty, B9600);
+    cfsetospeed(&tty, B9600);
+
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+        // return 1;
+    }
+
+     unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\n' };
      write(serial_port, msg, sizeof(msg));
-
-
-
 
 
 
