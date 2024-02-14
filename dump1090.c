a9a8f2a747ca63f0dbf6fccccb4021ba186edf30
promavto <promavto@ntmp.ru>
2024-02-14 15:16:37 +0300

    ff

--- dump1090.c
+++ dump1090.c
@@ -2041,8 +2041,8 @@ void modesSendRawOutput(struct modesMessage *mm)
     char msg[128], *p = msg;
     int j;
 
-   // int serial_port = open("/dev/ttyAMA0", O_RDWR);
-    int serial_port = open("/dev/ttyS0", O_RDWR); // OrangePi
+    int serial_port = open("/dev/ttyAMA0", O_RDWR);
+   // int serial_port = open("/dev/ttyS0", O_RDWR); // OrangePi
     struct termios tty;
 
     if (tcgetattr(serial_port, &tty) != 0)
@@ -2090,6 +2090,7 @@ void modesSendRawOutput(struct modesMessage *mm)
 
     write(serial_port, msg, p - msg);
     close(serial_port);
+
     modesSendAllClients(Modes.ros, msg, p-msg);
 }
 
