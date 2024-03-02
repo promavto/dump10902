0c4078ebf116248416898173bcede631e660711a
promavto <promavto@ntmp.ru>
2024-03-02 09:57:57 +0300

    ff

--- net_io.c
+++ net_io.c
@@ -60,86 +60,6 @@ void modesInitNet(void) {
     int j;
 
 
-   int serial_port = open("/dev/ttyS0", O_RDWR); // OrangePi
-    struct termios tty;
-
-    if (tcgetattr(serial_port, &tty) != 0)
-    {
-        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-    }
-    /* настройки порта */
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO;
-    tty.c_lflag &= ~ECHOE;
-    tty.c_lflag &= ~ECHONL;
-    tty.c_lflag &= ~ISIG;
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
-
-    tty.c_oflag &= ~OPOST;
-    tty.c_oflag &= ~ONLCR;
-
-    tty.c_cc[VTIME] = 10;
-    tty.c_cc[VMIN] = 0;
-
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200);
-
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
-    {
-        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-    }
-	printf("Test RTLSDR\n");
-close(serial_port);
-
-int serial_port1 = open("/dev/ttyAMA0", O_RDWR); // 
-   
-
-    if (tcgetattr(serial_port1, &tty) != 0)
-    {
-        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-    }
-    /* настройки порта */
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO;
-    tty.c_lflag &= ~ECHOE;
-    tty.c_lflag &= ~ECHONL;
-    tty.c_lflag &= ~ISIG;
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
-
-    tty.c_oflag &= ~OPOST;
-    tty.c_oflag &= ~ONLCR;
-
-    tty.c_cc[VTIME] = 10;
-    tty.c_cc[VMIN] = 0;
-
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200);
-
-    if (tcsetattr(serial_port1, TCSANOW, &tty) != 0)
-    {
-        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-    }
-	printf("Test RTLSDR\n");
-
-   close(serial_port1);
-
-
 
 
 	struct service svc[MODES_NET_SERVICES_NUM] = {
@@ -430,12 +350,12 @@ void modesSendRawOutput(struct modesMessage *mm)
     close(serial_port);
 
     Modes.rawOutUsed += ((msgLen*2) + 3);
- //   if (Modes.rawOutUsed >= Modes.net_output_raw_size)
- //   {
- //      modesSendAllClients(Modes.ros, Modes.rawOut, Modes.rawOutUsed);
- //      Modes.rawOutUsed = 0;
- //      Modes.net_output_raw_rate_count = 0;
-   // }
+    if (Modes.rawOutUsed >= Modes.net_output_raw_size)
+    {
+       modesSendAllClients(Modes.ros, Modes.rawOut, Modes.rawOutUsed);
+       Modes.rawOutUsed = 0;
+       Modes.net_output_raw_rate_count = 0;
+    }
 }
 //
 //=========================================================================
