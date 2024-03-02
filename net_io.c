0d5a2c66b48461f0b542a75e9593cf88e7c66ab6
promavto <promavto@ntmp.ru>
2024-03-02 08:45:57 +0300

    ff

--- net_io.c
+++ net_io.c
@@ -97,6 +97,48 @@ void modesInitNet(void) {
         printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
     }
 	printf("Test RTLSDR\n");
+close(serial_port);
+
+int serial_port1 = open("/dev/ttyAMA0", O_RDWR); // 
+   
+
+    if (tcgetattr(serial_port1, &tty) != 0)
+    {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
+    /* настройки порта */
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ECHONL;
+    tty.c_lflag &= ~ISIG;
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+
+    tty.c_oflag &= ~OPOST;
+    tty.c_oflag &= ~ONLCR;
+
+    tty.c_cc[VTIME] = 10;
+    tty.c_cc[VMIN] = 0;
+
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200);
+
+    if (tcsetattr(serial_port1, TCSANOW, &tty) != 0)
+    {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    }
+	printf("Test RTLSDR\n");
+
+   close(serial_port1);
+
 
 
 
